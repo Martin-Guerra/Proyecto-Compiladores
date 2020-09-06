feef8e50e78732cb58d0224720e71c234dcd8e0f
yago lacoste <yagolacoste>
2020-09-06 20:28:58 -0300

    Lectura de archivos

---
+++ Compilador/src/File/FileInput.java
@@ -0,0 +1,74 @@
+package File;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import javax.swing.JFileChooser;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class FileInput extends JFrame implements ActionListener {
+
+    private JTextField route;
+    private JButton btn;
+
+    public FileInput() {
+        super("JFileChooser Test");
+        setLayout(new FlowLayout());
+
+        route = new JTextField(30);
+        add(route);
+
+        btn = new JButton("Buscar...");
+        btn.addActionListener(this);
+        add(btn);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        
+        FileNameExtensionFilter imgFilter = new FileNameExtensionFilter("*.txt","txt");    
+        fileChooser.setFileFilter(imgFilter);
+
+        int result = fileChooser.showOpenDialog(this);
+
+        if (result != JFileChooser.CANCEL_OPTION) {
+
+            File fileName = fileChooser.getSelectedFile();
+
+            if ((fileName == null) || (fileName.getName().equals(""))) {
+            	route.setText("...");
+            } else {
+                route.setText(fileName.getAbsolutePath());
+                FileReaderSource fileReader=new FileReaderSource(this.getRoute().getText());
+            }
+        }
+        
+      
+    }
+
+    public JTextField getRoute() {
+		return route;
+	}
+
+
+	public static void main(String[] args) {
+        FileInput test = new FileInput();
+        test.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        test.setSize(400, 110);
+        test.setVisible(true);
+       
+    }
+}
+
---
+++ Compilador/src/File/FileReaderSource.java
@@ -0,0 +1,47 @@
+package File;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileReaderSource {
+	  //Declarar una variable BufferedReader
+    private  BufferedReader br = null;
+    
+    
+    public FileReaderSource(String route) {
+     try {
+        //Crear un objeto BufferedReader al que se le pasa 
+        //   un objeto FileReader con el nombre del fichero
+        br = new BufferedReader(new FileReader(route));
+        //Leer la primera línea, guardando en un String
+        String texto = br.readLine();
+        //Repetir mientras no se llegue al final del fichero
+        while(texto != null)
+        {
+            //Hacer lo que sea con la línea leída
+            System.out.println(texto);
+            //Leer la siguiente línea
+            texto = br.readLine();
+        }
+     }
+     catch (FileNotFoundException e) {
+         System.out.println("Error: Fichero no encontrado");
+         System.out.println(e.getMessage());
+     }
+     catch(Exception e) {
+         System.out.println("Error de lectura del fichero");
+         System.out.println(e.getMessage());
+     }
+     finally {
+         try {
+             if(br != null)
+                 br.close();
+         }
+         catch (Exception e) {
+             System.out.println("Error al cerrar el fichero");
+             System.out.println(e.getMessage());
+         }
+     }
+    }
+}
