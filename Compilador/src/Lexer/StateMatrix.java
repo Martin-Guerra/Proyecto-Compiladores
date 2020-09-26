9a32d10ae6ae0435b5372f05cb1234925bd6fd8b
Yago Lacoste <yagolacoste>
2020-09-26 20:19:26 -0300

    fin de las acciones semanticas y arregle lo de tincho

--- Compilador/src/Lexer/StateMatrix.java
+++ Compilador/src/Lexer/StateMatrix.java
@@ -4,8 +4,9 @@ import java.util.HashMap;
 import semantic_Actions.*;
 
 public class StateMatrix {
+	private static final int EOF = 9999999;
 	private State[][] matrix;
-	private static final int ROW = 20;
+	private static final int ROW = 19;
 	private static final int COLUMN =  28;
 	private HashMap mapofcaracters;
 	
@@ -16,15 +17,16 @@ public class StateMatrix {
 		for(int i = 0; i < ROW; i++) {
 			for(int j = 0; j < COLUMN; j++) {
 				if( i == 0 || i == 1 || i == 5 || i == 8 || i == 10 || i == 11 || i == 15
-					|| i == 17 || i == 18 || i == 19 ) {
+					|| i == 16 || i == 17 || i == 18 ) {
 					this.matrix[i][j] = new State(-1);
 				}
-				if(i == 2 || i == 3 || i == 4 || i == 6 || i == 7 || i == 8)
+				if(i == 2 || i == 3 || i == 4 || i == 6 || i == 7 || i == 9)
 					this.matrix[i][j] = new State(0);
-				if(i == 13 || i == 14)
-					this.matrix[i][j] = new State(13);
-				if(i == 15)
-					this.matrix[i][j] = new State(15);
+				
+				if(i == 12 || i == 13)
+					this.matrix[i][j] = new State(12);
+				if(i == 14)
+					this.matrix[i][j] = new State(14);
 			}
 		}
 		
@@ -34,16 +36,16 @@ public class StateMatrix {
 		matrix[0][2].setState(1);
 		matrix[0][3].setState(1);
 		matrix[0][4].setState(1);
-		matrix[0][5].setState(10);
+		matrix[0][5].setState(9);
 		matrix[0][6].setState(0);
-		matrix[0][7].setState(6);
-		matrix[0][8].setState(12);
+		matrix[0][7].setState(5);
+		matrix[0][8].setState(11);
 		matrix[0][9].setState(0);
-		matrix[0][10].setState(15);
-		matrix[0][20].setState(16);
-		matrix[0][21].setState(17);
-		matrix[0][22].setState(18);
-		matrix[0][23].setState(19);
+		matrix[0][10].setState(14);
+		matrix[0][20].setState(15);
+		matrix[0][21].setState(16);
+		matrix[0][22].setState(17);
+		matrix[0][23].setState(18);
 		matrix[0][24].setState(0);
 		matrix[0][25].setState(0);
 		matrix[0][26].setState(0);
@@ -59,59 +61,54 @@ public class StateMatrix {
 		//Fila 2
 		matrix[2][1].setState(2);
 		matrix[2][6].setState(3);
-		matrix[2][7].setState(6);
-		matrix[2][27].setState(-1);
+		matrix[2][7].setState(5);
+		
 		
 		//Fila 3		
 		matrix[3][3].setState(4);
-		matrix[3][27].setState(-1);
+		
 		
 		//Fila 4
-		matrix[4][4].setState(5);
-		matrix[4][27].setState(-1);
+		matrix[4][4].setState(-1);
 		
-		//Fila 5 ya cargada toda con estado final
+		
+		//Fila 5
+		matrix[5][1].setState(5);
+		matrix[5][2].setState(6);
 		
 		//Fila 6		
-		matrix[6][1].setState(6);
-		matrix[6][2].setState(7);
+		matrix[6][11].setState(7);
+		matrix[6][19].setState(7);
 		
 		//Fila 7
-		matrix[7][11].setState(8);
-		matrix[7][19].setState(8);
-		matrix[7][27].setState(-1);
+		matrix[7][1].setState(8);
 		
 		//Fila 8
-		matrix[8][1].setState(9);
-		matrix[8][27].setState(-1);
+		matrix[8][1].setState(8);
 		
 		//Fila 9
 		matrix[9][1].setState(9);
+		matrix[9][6].setState(10);
 		
 		//Fila 10
 		matrix[10][5].setState(10);
-		matrix[10][6].setState(11); 
-		matrix[10][27].setState(-1);
 		
 		//Fila 11
-		matrix[11][5].setState(11);
+		matrix[11][9].setState(12);
 		
 		//Fila 12
 		matrix[12][9].setState(13);
 		
 		//Fila 13
-		matrix[13][9].setState(14);
-		matrix[13][27].setState(-1);
+		matrix[13][8].setState(0);
+		matrix[13][9].setState(3);
+		
 		
 		//Fila 14
-		matrix[14][8].setState(0);
-		matrix[14][9].setState(14);
-		matrix[14][27].setState(-1);
+		matrix[14][18].setState(-1);
+		matrix[14][26].setState(0);
 		
-		//Fila 15
-		matrix[15][10].setState(-1);
-		matrix[15][26].setState(0);
-		matrix[15][27].setState(-1);
+		
 		
 		
 		
@@ -130,6 +127,8 @@ public class StateMatrix {
 		SemanticAction sa11 = new SemanticAction11();
 		SemanticAction sa12 = new SemanticAction12();
 		SemanticAction sa13 = new SemanticAction13();
+		SemanticAction sa14 = new SemanticAction13();
+		SemanticAction sa15 = new SemanticAction13();
 		
 		/*ERR_ID errId = new ERR_ID();
 		ERR_CTN errCte = new ERR_CTN();
@@ -139,93 +138,101 @@ public class StateMatrix {
 		for(int i = 0; i < ROW; i++) {
 			for(int j = 0; j < COLUMN - 1; j++) {
 				if(i == 0)
-					matrix[i][j].setSemanticAction(sa3);
-				if(i == 1)
 					matrix[i][j].setSemanticAction(sa5);
-				if(i == 2 || i == 3 || i == 4 || i == 7 || i == 8 || i == 10)
-					matrix[i][j].setSemanticAction(sa1);
+				if(i == 1)
+					matrix[i][j].setSemanticAction(sa2);
+				if(i == 2 || i == 3 || i == 4 || i == 6 || i == 7 || i == 9)
+					matrix[i][j].setSemanticAction(sa12);
 				if(i == 5)
 					matrix[i][j].setSemanticAction(sa6);
-				if(i == 6)
-					matrix[i][j].setSemanticAction(sa13);
-				if(i == 9)
-					matrix[i][j].setSemanticAction(sa10);
-				if(i == 11)
-					matrix[i][j].setSemanticAction(sa7);
-				if(i == 12 || i == 16 || i == 17 || i ==18 || i == 19)
-					matrix[i][j].setSemanticAction(sa11);
-				if(i == 13 || i == 14 || i == 15)
+				if(i == 8)
+					matrix[i][j].setSemanticAction(sa3);
+				if(i == 10)
 					matrix[i][j].setSemanticAction(sa4);
+				if(i == 11)
+					matrix[i][j].setSemanticAction(sa1);
+				if(i == 12 || i == 13 || i == 14 )
+					matrix[i][j].setSemanticAction(sa5);
+				if(i == 15 || i == 16 || i == 17 || i == 18)
+					matrix[i][j].setSemanticAction(sa1);
 			}
 		}
 		
 		//Fila 0
-		matrix[0][9].setSemanticAction(sa1);
-		for(int j = 11; j <= 19; j++) {
-			matrix[0][j].setSemanticAction(null);
+		for(int j = 0; j <= 5; j++) {
+			matrix[0][j].setSemanticAction(sa5);
 		}
-		matrix[0][24].setSemanticAction(sa1);
-		matrix[0][25].setSemanticAction(sa2);
-		matrix[0][26].setSemanticAction(sa2);
+		matrix[0][6].setSemanticAction(sa12);
+		matrix[0][7].setSemanticAction(sa5);
+		matrix[0][8].setSemanticAction(sa5);
+		matrix[0][9].setSemanticAction(sa12);
+		matrix[0][24].setSemanticAction(sa12);
+		matrix[0][25].setSemanticAction(sa13);
+		matrix[0][26].setSemanticAction(sa13);
+		
 		
 		//Fila 1
 		for(int j = 0; j <= 4; j++) {
-			matrix[1][j].setSemanticAction(sa3);
+			matrix[1][j].setSemanticAction(sa5);
 		}
-		matrix[1][6].setSemanticAction(sa4);
+		matrix[1][5].setSemanticAction(sa2);
+		matrix[1][6].setSemanticAction(sa5);
+		
 		
 		//Fila 2
-		matrix[2][1].setSemanticAction(sa4);
-		matrix[2][6].setSemanticAction(sa9); 
-		matrix[2][7].setSemanticAction(sa10);
+		matrix[2][1].setSemanticAction(sa5);
+		matrix[2][6].setSemanticAction(sa5); 
+		matrix[2][7].setSemanticAction(sa5);
 		
 		//Fila 3		
-		matrix[3][3].setSemanticAction(sa4);
+		matrix[3][3].setSemanticAction(sa5);
 		
 		//Fila 4
-		matrix[4][4].setSemanticAction(sa4); 
+		matrix[4][4].setSemanticAction(sa7); 
 		
 		//Fila 5 ya cargada
+		matrix[5][1].setSemanticAction(sa5); 
+		matrix[5][2].setSemanticAction(sa5); 
 		
 		//Fila 6
-		matrix[6][1].setSemanticAction(sa4); 
-		matrix[6][2].setSemanticAction(sa4); 
+		matrix[6][11].setSemanticAction(sa5); 
+		matrix[6][19].setSemanticAction(sa5); 
 		
 		//Fila 7
-		matrix[7][11].setSemanticAction(sa4);
-		matrix[7][19].setSemanticAction(sa4);
+		matrix[7][1].setSemanticAction(sa5);
+		
 		
 		//Fila 8
-		matrix[8][1].setSemanticAction(sa4);
+		matrix[8][1].setSemanticAction(sa5);
 		
 		//Fila 9
-		matrix[9][1].setSemanticAction(sa4);
+		matrix[9][5].setSemanticAction(sa5);
+		matrix[9][6].setSemanticAction(sa5);
 		
 		//Fila 10
 		matrix[10][5].setSemanticAction(sa5);
 		
 		//Fila 11
-		matrix[11][5].setSemanticAction(sa4);
+		matrix[11][9].setSemanticAction(sa5);
 		
-		//Fila 12
-		matrix[12][9].setSemanticAction(sa4);
+		//file 12 cargada
 		
 		//Fila 13 ya cargada
+		matrix[13][8].setSemanticAction(sa9);
 		
 		//Fila 14
-		matrix[14][8].setSemanticAction(sa8);
+		matrix[14][10].setSemanticAction(sa11);
+		matrix[14][26].setSemanticAction(sa14);
 		
-		//Fila 15
-		matrix[15][18].setSemanticAction(sa6); 
-		matrix[15][26].setSemanticAction(sa1);
-		
-		
-		//Fila 16
-		for(int i = 16; i < ROW; i++) {
-			matrix[i][22].setSemanticAction(sa12);
+			
+		//Fila 15 en adelante
+		for(int i = 15; i < ROW; i++) {
+			matrix[i][22].setSemanticAction(sa10);
 		}
+		
 		for(int i = 0; i < ROW; i++) {
-			matrix[i][27].setSemanticAction(null);
+			matrix[i][27].setNextstate(EOF);
+			matrix[i][27].setSemanticAction(sa15);
 		}
 		
 		mapofcaracters = new HashMap();
